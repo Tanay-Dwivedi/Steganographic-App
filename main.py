b14db219ce376b476c78623fdb2e598e26516f8c
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-02-02 23:15:28 +0530

    text encrytption added

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import streamlit as sl
 from stegano import lsb
 from PIL import Image
 
-sl.title("Steganographic")
+sl.set_page_config(page_title="Steganographic App", page_icon="🧐")
+
+sl.title("Steganographic App")
+sl.write("##")
+
+sl.write("### To make a secret image")
 
 with sl.form("Upload your image"):
     upload_img = sl.file_uploader("Upload your image", accept_multiple_files=False)
@@ -12,22 +17,40 @@ with sl.form("Upload your image"):
 
 sl.write("##")
 
+
+def text_to_secret_message(org_text):
+    binary_representation = " ".join(format(ord(char), "08b") for char in org_text)
+    return binary_representation
+
+
+def secret_to_text_message(sec_text):
+    binary_values = sec_text.split()
+    ascii_characters = [chr(int(binary, 2)) for binary in binary_values]
+    original_string = "".join(ascii_characters)
+    return original_string
+
+
 if submit_btn:
     sl.write("### Your secret image")
-    img_name = img_name + ".png"
+    new_img_name = img_name.replace(" ", "_")
+    new_img_name = new_img_name + ".png"
+    input_text = text_to_secret_message(input_text)
     secret = lsb.hide(upload_img, input_text)
-    secret.save(img_name)
-    sl.image(img_name)
-    image = Image.open(img_name)
+    secret.save(new_img_name)
+    sl.image(new_img_name)
+    image = Image.open(new_img_name)
     sl.write("##")
 
-    with open(img_name, "rb") as file:
+    with open(new_img_name, "rb") as file:
         btn = sl.download_button(
             label="Download image",
             data=file,
-            file_name=img_name,
+            file_name=new_img_name,
             mime="image/png",
         )
+
+sl.write("##")
+sl.write("### To find the secret message")
 
 with sl.form("Upload the secret image"):
     upload_img = sl.file_uploader(
@@ -39,5 +62,4 @@ sl.write("##")
 
 if submit_btn:
     sl.write("### Your secret message is :")
-    sl.write("##")
-    sl.write(lsb.reveal(upload_img))
+    sl.write(f"**{secret_to_text_message(lsb.reveal(upload_img))}**")
